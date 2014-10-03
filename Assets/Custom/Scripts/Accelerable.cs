53e68a88a270d14755670f02c095aeed499ba133
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2014-10-03 15:46:28 -0400

    Initial project copy

---
+++ Assets/Custom/Scripts/Accelerable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CarController))]
+public class Accelerable : MonoBehaviour {
+
+    private CarController carController;
+    private float baseSpeed;
+    private float baseTorque;
+
+    public float accelTime = 2;
+
+	// Use this for initialization
+	void Start () {
+        this.carController = this.GetComponent<CarController>();
+        this.baseSpeed = this.carController.MaxSpeed;
+        this.baseTorque = this.carController.MaxTorque;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerable Accelerate(float speedFactor)
+    {
+        this.carController.MaxSpeed = this.baseSpeed * speedFactor;
+        this.carController.MaxTorque = this.baseTorque * speedFactor;
+        yield return new WaitForSeconds(this.accelTime);
+        this.carController.MaxSpeed = this.baseSpeed;
+        this.carController.MaxTorque = this.baseTorque;
+    }
+}
---
+++ Assets/Custom/Scripts/Accelerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Accelerator : MonoBehaviour {
+
+    public float speedFactor = 2;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.LogWarning("Colliding");
+        Accelerable accelerable = other.GetComponentInParent<Accelerable>();
+        if (accelerable)
+        {
+            Debug.LogWarning("Accelerate !");
+            accelerable.Accelerate(this.speedFactor);
+        }
+    }
+}
