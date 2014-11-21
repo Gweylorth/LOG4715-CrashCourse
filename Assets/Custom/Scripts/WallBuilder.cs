5ea27ef9260085c778d2cc59af75e37e9e71f5c4
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2014-11-21 18:33:39 -0500

    Complete reskin

---
+++ Assets/Custom/Scripts/WallBuilder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBuilder : MonoBehaviour {
+
+    public Transform brick;
+    public int repeats;
+
+    void Start()
+    {
+        for (int y = 0; y < repeats; y++)
+        {
+            for (int x = 0; x < repeats; x++)
+            {
+                Instantiate(brick, new Vector3(this.transform.position.x + x, this.transform.position.y + y, 0), this.transform.rotation);
+            }
+        }
+    }
+}
