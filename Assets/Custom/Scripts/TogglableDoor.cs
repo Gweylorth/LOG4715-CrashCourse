b2115b76c1f27db23483cebdad5ab247a199e26f
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2014-11-30 23:19:55 -0500

    Add new SkyBox and TogglableDoor

---
+++ Assets/Custom/Scripts/TogglableDoor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglableDoor : MonoBehaviour {
+
+    private bool state = false;
+    private float currentAngle = 0;
+    private Transform leftSide;
+    private Transform rightSide;
+
+    void Start()
+    {
+        this.leftSide = this.transform.FindChild("LeftSide");
+        this.rightSide = this.transform.FindChild("RightSide");
+    }
+
+    void FixedUpdate()
+    {
+
+        float step = 50 * Time.deltaTime;
+        if (this.state && this.currentAngle < 90)
+        {
+            this.leftSide.FindChild("Grid").Rotate(this.leftSide.FindChild("Base").right, -step);
+            this.rightSide.FindChild("Grid").Rotate(this.rightSide.FindChild("Base").right, -step);
+            this.currentAngle += step;
+        }
+        if (!this.state && this.currentAngle > 0)
+        {
+            this.leftSide.FindChild("Grid").Rotate(this.leftSide.FindChild("Base").right, step);
+            this.rightSide.FindChild("Grid").Rotate(this.rightSide.FindChild("Base").right, step);
+            this.currentAngle -= step;
+        }
+    }
+
+    public void SwitchState()
+    {
+        this.state = !this.state;
+    }
+}
---
+++ Assets/Custom/Scripts/Toggle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toggle : MonoBehaviour {
+
+    public Material onMaterial;
+    public Material offMaterial;
+    public TogglableDoor target;
+
+    private bool state = false;
+
+    // Called when the collider enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Style")
+        {
+            this.state = !this.state;
+            this.GetComponent<MeshRenderer>().material = (this.state) ? this.onMaterial : this.offMaterial;
+            // Make noise
+            AudioSource audio = this.gameObject.GetComponent<AudioSource>();
+            if (audio)
+            {
+                audio.Play();
+            }
+            this.target.SwitchState();
+        }
+    }
+}
